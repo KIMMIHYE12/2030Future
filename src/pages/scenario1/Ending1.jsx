7ead92f5758ec0aef49db800555ffd97e44055d4
sapiens12 <sapiens12@nate.com>
2025-07-04 12:08:37 +0200

    ending

--- src/pages/scenario1/Ending1.jsx
+++ src/pages/scenario1/Ending1.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Ending1 = () => {
-  return <div>ending1</div>;
+  return (
+    <div className='ending-screen'>
+      <div>
+        <p className='ending-text'>
+          Und so endete Klaus’ Tag mit einem Lächeln.
+          <br />
+          Er hat seiner Enkelin Anna geschrieben und war zufrieden.
+        </p>
+
+        <p className='ending-finish'>Ende.</p>
+      </div>
+      <button className='home-button' onClick={() => navigate("/")}>
+        Zurück zur Startseite
+      </button>
+    </div>
+  );
 };
 
 export default Ending1;
--- src/pages/scenario1/Ending2.jsx
+++ src/pages/scenario1/Ending2.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Ending2 = () => {
-  return <div>Ending2</div>;
+  return (
+    <div className='ending-screen'>
+      <div>
+        <p className='ending-text'>
+          Und so endete Klaus’ Tag mit einem Lächeln.
+          <br />
+          Er schrieb Anna eine liebe Nachricht – und war zufrieden.
+        </p>
+
+        <p className='ending-finish'>Ende.</p>
+      </div>
+      <button className='home-button' onClick={() => navigate("/")}>
+        Zurück zur Startseite
+      </button>
+    </div>
+  );
 };
 
 export default Ending2;
